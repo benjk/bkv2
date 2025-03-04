ecb38bbf5a8223be8316ca0ec918c79289b66044
BenKielinski <benjamin.kielinski@hotmail.fr>
2025-03-04 17:15:13 +0100

    rollback on publi mentions / vite way

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { resolve } from 'path';
 export default defineConfig({
   root: 'src',
   publicDir: resolve(__dirname, 'public'),
+  input: {
+    main: resolve(__dirname, 'index.html'),
+    mentions: resolve(__dirname, 'mentions-legales.html')
+  },
   build: {
     outDir: '../dist',
   }
